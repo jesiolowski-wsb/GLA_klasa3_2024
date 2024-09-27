81601a8613aec01b0fd53b8d26cbde350a918e6e
filip.jesiolowski.sp <filip.jesiolowski.sp@lhsystems.com>
2024-09-27 20:57:02 +0200

    Sprawdzona Lena

--- sprawdzian-2024-09/lenahasse/zad2.py
+++ sprawdzian-2024-09/lenahasse/zad2.py
@@ -4,7 +4,7 @@ def fizzbuzz_v2(n):
     for i in range (1,n+1):
         print(i, end=" ")
         if i%3==0:
-         print(i, 'Fizz', end='')
+         print('Fizz', end='')
          # tutaj problemem jest wcięcie - IF w IFie tj. linia wykonuje sie
          # tylko w obrębie tych podzielnych przez 3.
          # chyba lepiej do wcięć używac TABów a nie spacji
