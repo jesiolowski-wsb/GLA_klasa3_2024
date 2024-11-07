9ee0fdb8f5f10bd2f76bf87b223473f500c1cbe3
filip.jesiolowski.sp <filip.jesiolowski.sp@lhsystems.com>
2024-11-07 18:50:07 +0100

    poprawiony Michal

--- sprawdzian-2024-10/Michal_Stanulewicz/Michal_Stanulewicz.py
+++ sprawdzian-2024-10/Michal_Stanulewicz/Michal_Stanulewicz.py
@@ -1,29 +1,42 @@
-zad 1
-plik = open('plik.txt', 'r')
+# suma: 15pkt ALE:
+# -1pkt za wszystkie odpowiedzi do zadań w jednym pliku
+
+# Zdobyte punkty: 3/6
+# zad 1
+# błędna nazwa pliku - powinien nazywać się 'liczby.txt'
+plik = open('liczby.txt', 'r')
 zapis = plik.read()
+
+# algorytm zadziała, ale nie widzę nigdzie jego wywołania z danymi z pliku
 def binaryzacja(zapis):
     wynik = ''
     while zapis > 0:
         wynik = str(zapis % 2) + wynik
         zapis //= 2
     return wynik
-zad 2
-ligma=open("tekscik.txt", 'w')
+
+
+# Zdobyte punkty: 3/5
+# zad 2
+# błędna nazwa pliku - powinien nazywać się tekst.tst
+ligma=open("tekst.txt", 'w')
+# nie rozumiem powodu dla którego to zmienna z wartością a nie treść pliku
 sigma=("Ania i Tomek poszli do kina . Potem kupili lody.")
 x=sigma.split()
-
 nowa_lista = []
 
-
 for element in x:
-
     if len(element) > 3 and element[0].isupper():
         nowa_lista.append(element)
 wynik = "\n".join(nowa_lista)
+# brak zapisu do pliku
 print(wynik)
 ligma.write(wynik)
 ligma.close()
-zad 3
+
+
+# Zdobyte punkty: 4/4
+# zad 3
 import random
 def generuj_id():
     litery = ''.join(random.choices('ABCDE', k=2))
@@ -33,10 +46,19 @@ def generuj_id():
         plik.write(nowe_id + '\n')
     return nowe_id
 print(generuj_id())
-zad4
+
+
+# Zdobyte punkty: 5/7
+# zad4
 with (open ("dane.txt","r") as plik):
+ # swietny sposob na wrzucenie elementów do listy
  binarne= plik.read().splitlines()
+
+ # fajny sposób umieszczenia wyrażenia listy, ale w sprawdzeniu czy liczba jest parzysta, brakuje faktycznego sprawdzenia
+ # int(b,2) zwroti true dla kazdej wartosci jako sukces wykonania operacji rzutowania na wartosc binarna
  parzyste=[int(b, 2) for b in binarne if int(b, 2)]
+
+ # niestety join łączy elementy string a nie int:(
  o="\n".join(parzyste)
 print(o)
 f = open("dane.txt", "w")
