cda2677e0436413a095452eefd50abe103c30382
Chemigus <120684216+Chemigus@users.noreply.github.com>
2024-12-06 07:52:17 +0100

    Added popular.txt downloading

--- projekt-gry-w-wisielca/Jastrzebski_Kuba/main.py
+++ projekt-gry-w-wisielca/Jastrzebski_Kuba/main.py
@@ -1,15 +1,31 @@
+from urllib.request import urlopen
 from random import randrange
+from os.path import isfile
+
+
+if not isfile('./popular.txt'):
+    with urlopen( 'https://raw.githubusercontent.com/dolph/dictionary/refs/heads/master/popular.txt' ) as webpage:
+        content = webpage.read().decode()
+    with open('popular.txt', 'w') as output:
+        output.write(content)
+
 with open("popular.txt", "r") as file:
     file.seek(randrange(1, 185189))
     file.readline()
     original_answer = list(file.readline().strip())
+
+guessed_words = []
 lives = 7
 win_status = False
 active_answer = ["_"]*len(original_answer)
+
+
 print("".join(active_answer))
 print("Lives:", lives)
+
+
 while True:
-    guess = input("Zgadnij literę lub słowo: ")
+    guess = input("Zgadnij literę lub słowo: ").lower()
 
     if len(guess) > 1:
         if list(guess) == original_answer:
@@ -19,29 +35,25 @@ while True:
             lives -= 1
 
     elif len(guess) == 1:
-
-        if original_answer.count(guess) > 0 and active_answer.count(guess) == 0:
+        if guess not in guessed_words: guessed_words.append(guess)
+        if guess in original_answer and guess not in active_answer:
             locations = [index for index in range(len(original_answer)) if original_answer[index] == guess]
             for index in locations:
                 active_answer[index] = guess
 
-            '''counter = original_answer.count(guess)
-            print(counter)
-            start_counting = 0
-            while counter > 0:
-                print(counter)
-                active_answer[original_answer[start_counting:].index(guess)] = guess
-                start_counting = original_answer[start_counting:].index(guess)
-                counter -= 1'''
         else: lives -= 1
 
     print("".join(active_answer))
     print("Lives:", lives)
+    print(f"Guessed words: {", ".join(guessed_words)}")
+
     if active_answer == original_answer:
         win_status = True
         break
+
     if lives < 1:
         win_status = False
         break
+
 if win_status: print("You won!")
 else: print("You lost!\nThe answer was:", "".join(original_answer))
