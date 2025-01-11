6bc958dca32e9cdf607e71da6ce2d302455dc9e1
filip.jesiolowski.sp <filip.jesiolowski.sp@lhsystems.com>
2025-01-11 22:07:46 +0100

    poprawiony Kuba

--- sprawdzian-2025-01/termin_1/Jakub_Jastrzebski.py
+++ sprawdzian-2025-01/termin_1/Jakub_Jastrzebski.py
@@ -1,8 +1,15 @@
+# suma zdobytych punktow: 21/20
+# zad.1: 6/6
+# zad.2: 6/6
+# zad.3: 9/8
+
+# wystarczyłoby 'return a and b' :)
 def AND(a, b):
     if a and b:
         return True
     return False
 
+# wystarczyłoby 'return a or b' :)
 def OR(a, b):
     if a or b:
         return True
@@ -14,11 +21,21 @@ def NOT(a):
 def wyswietl_tabele_prawdy():
     print("a     b     AND    OR     NOT(a)\n"
           "-----------------------------------")
+    # troszkę czytelniej byłoby po prostu 'in [False, True]'
+    # range(2) wymaga pomyślenia że:
+    # - liczymy od 0, wiec bedzie 0 i 1
+    # - 0 i 1 to integer, ale gdy spojrze w funkcje AND czy OR to tam sa wartosci boolowe
+    # - czyli tutaj pewnie chodzi o rzutowanie 0 i 1 na wartosci boolowe
+    # kupa roboty dla mózgu
+    # w wolnej chwili rzuc okiem na artykul https://minds.md/zakirullin/cognitive
+    # to o innym jezyku, ale samo pojęcie 'cognitive load' jest tu istotne
     for a in range(2):
         for b in range(2):
             print(f"{a}{b:6}{AND(a,b):6}{OR(a,b):7}{NOT(a):7}")
 
 def COMPLEX(a, b, c):
+    # swietnie przyuwazone ze nie trzeba `return (a and b) or (not c)` :)
+    # gratki
     return OR(AND(a,b), NOT(c))
 
 
