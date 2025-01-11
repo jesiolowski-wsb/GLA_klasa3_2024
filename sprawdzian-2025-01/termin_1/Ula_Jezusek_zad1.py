ccf0b20f5a8cf46a3569d932ae8e4b2d2563b823
filip.jesiolowski.sp <filip.jesiolowski.sp@lhsystems.com>
2025-01-11 23:10:10 +0100

    poprawiona Ula

--- sprawdzian-2025-01/termin_1/Ula_Jezusek_zad1.py
+++ sprawdzian-2025-01/termin_1/Ula_Jezusek_zad1.py
@@ -1,3 +1,8 @@
+# suma zdobytych punktow: 8/20
+# zad.1: 6/6
+# zad.2: 2/6
+# zad.3: 0/8
+
 def bramki_and(a , b):
     return a and b
 
@@ -5,6 +10,7 @@ def bramki_and(a , b):
 
 print(bramki_and(True, False))
 
+# nietypowy sposób formatowania - enter po rozpoczęciu funkcji
 def bramki_or(a, b):
 
      return a or b
