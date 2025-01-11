681ca6bdd3a6798b211fb40a4f04900186b33a35
filip.jesiolowski.sp <filip.jesiolowski.sp@lhsystems.com>
2025-01-11 23:19:12 +0100

    poprawiony Witek

--- sprawdzian-2025-01/termin_1/Witek_Sumila.py
+++ sprawdzian-2025-01/termin_1/Witek_Sumila.py
@@ -1,5 +1,16 @@
+# suma zdobytych punktow: 0/20
+# zad.1: 6/6
+# zad.2: 6/6
+# zad.3: 0/8
+
+# te a i b sa tutaj kompletnie niepotrzebne
 a=1
 b=2
+
+# istnieje kilka konwencji pisania nazw zmiennych / funkcji
+# https://www.freecodecamp.org/news/snake-case-vs-camel-case-vs-pascal-case-vs-kebab-case-whats-the-difference/
+# w pythonie popularne jest pisanie funkcji albo jako camelCase, albo jako snake_case
+# więc tutaj widziałby albo bramka_and lub bramkaAnd
 def bramkaand(a,b):
     return a and b
 
@@ -9,8 +20,12 @@ def bramkaor(a,b):
 def bramkanot(a):
     return not a
 
+# powtórzenie niepotrzebnych zmiennych a i b
 a=1
 b=2
+
+# powtórzenie funkcji z zadania 1 -
+# redefiniowanie funkcji nie jest dozwolone w zadnym z jezykow programowania
 def bramkaand(a,b):
     return a and b
 
@@ -19,10 +34,12 @@ def bramkaor(a,b):
 
 def bramkanot(a):
     return not a
+
 
 def wyswietl_tabele_prawdy():
     print("a    b    and     or     not(a)")
     print('--------------------------------')
+    # absolutnie gratuluje dobrego podejscia do listowania elementów listy
     for a in [False, True]:
         for b in[False, True]:
             print(int(a),int(b), int(bramkaand(a,b)),int(bramkaor(a,b)),int(bramkanot(a)))
