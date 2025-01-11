8a2e3ce43cd2f49589c7a5f4eac1cb2f412b2ff4
filip.jesiolowski.sp <filip.jesiolowski.sp@lhsystems.com>
2025-01-11 22:13:40 +0100

    poprawiona Julia

--- sprawdzian-2025-01/termin_1/Julia_Tyrakowska.py
+++ sprawdzian-2025-01/termin_1/Julia_Tyrakowska.py
@@ -1,3 +1,8 @@
+# suma zdobytych punktow: 8/20
+# zad.1: 6/6
+# zad.2: 2/6
+# zad.3: 0/8
+
 def bramki_and(a,b):
     return a and b
 
@@ -11,6 +16,8 @@ def bramki_not(a):
     return not a
 print(bramki_not(True))
 
+# szkoda że zabrakło implementacji pętli która by odpalała tabelę
+# rzuć okiem na sugerowane rozwiązanie na https://github.com/jesiolowski-wsb/GLA_klasa3_2024/blob/main/sprawdzian-2025-01/termin_1/readme.md
 def wyswietl_tabele_prawdy():
     print("a    b    AND    OR     NOT(a)")
     print("-----------------------------------")
